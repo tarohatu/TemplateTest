c4fed9ca125dbf062352d0120c6aaee1971fa32a
tarohatu <tarohatu2@gmail.com>
2019-12-27 09:53:06 +0900

    initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+const title = 'React with Webpack and Babel';
+ReactDOM.render(
+  <div>aaaaaaaaaaa</div>,
+  document.getElementById('app')
+);
+
+module.hot.accept();
